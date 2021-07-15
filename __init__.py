1ca19e81738034ba011ad9eeb373f92f15a9dc3c
Zack <zcommon@streamlounge.io>
2021-07-14 22:00:35 -0700

    Changed mod name

--- __init__.py
+++ __init__.py
@@ -9,10 +9,10 @@ from modloader.modclass import Mod, loadable_mod
 @loadable_mod
 class AWSWMod(Mod):
     def mod_info(self):
-        return ("Test", "v0.0.1", "Eval")
+        return ("This Man Owes me Ice Cream! Remy Edition", "v0.5.0", "Eval")
 
     def mod_load(self):
-        #Remy's ending hook
+        #Remy's ending hook - Note I need to push this back earlier to change some prior dialogue
         source = ml.search_peak_if(modast.find_say("Besides, if you really end up going back in time, I'll see you again."), ast.Scene, 100)
         test_hook = modast.find_label("eval_tmomi_common")
         hook = modast.hook_opcode(source, None)
